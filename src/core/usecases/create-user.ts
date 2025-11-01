7d02fb47df955f3e77516941734fdc490481f649
Victorino Pedro <vpangola@gmail.com>
2025-11-01 18:06:35 +0100

    Caso seja fornecido um nome com números

--- src/core/usecases/create-user.ts
+++ src/core/usecases/create-user.ts
@@ -3,11 +3,12 @@ import type { User } from "../entities/user.js";
 
 export class CreateUser implements ICreateUser{
     async execute({ user }: { user: User; }): Promise<string> {
-        
         if(user.getName() === undefined || user.getName() === null)
-            throw new Error("Nome informado invalido. 2");
-        if(user.getName().trim.length === 0)
-            throw new Error("Nome informado invalido.");
+            throw new Error("O campo nome não ser vazio.");
+        if(user.getName().trim().length === 0)
+            throw new Error("O campo nome não ser vazio.");
+        if(/\d/.test(user.getName()))
+            throw new Error("O campo nome não pode conter números");
         
 
         return 'Utilizador criádo com sucesso.'
--- src/test/create-user.test.ts
+++ src/test/create-user.test.ts
@@ -3,11 +3,18 @@ import { CriarUtilizadorContrioller } from "../adapters/in/index.js";
 import { User } from "../core/entities/user.js";
 import { CreateUser } from "../core/usecases/create-user.js";
 
-describe("Quando for criar um utilizador", () =>{
+describe("Quando for criar um utilizador", () => {
 
     const user = new User('', '', '', '');
-     it('Caso seja fornecido um nome inválido', () =>{
-        const promise = new CriarUtilizadorContrioller(new CreateUser()).criar({user});
-         expect(promise).rejects.toThrowError('Nome informado invalido.');
+
+    it('Caso seja fornecido um nome inválido', () => {
+        const promise = new CriarUtilizadorContrioller(new CreateUser()).criar({ user });
+        expect(promise).rejects.toThrowError('O campo nome não ser vazio.');
+    })
+
+    it('Caso seja fornecido um nome com números', () => {
+        user.setName('VP5')
+        const promise = new CriarUtilizadorContrioller(new CreateUser()).criar({ user });
+        expect(promise).rejects.toThrowError('O campo nome não pode conter números');
     })
 })
